d3c5fbb81376a8ea218bb4e160b41528d513aa86
Daniel Murray <danielmurray@Daniels-MacBook-Pro.local>
2018-02-27 20:33:29 -0800

    near completion modal issues

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+module.exports = {
+	array: [
+		{
+			name: 'Tom',
+			photo: '/images/mypic.jpg',
+			scores: ['1','1','5','1','1','1','1','1','1','1']
+		},
+
+		{
+  			name: 'Ahmed',
+  			photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  			scores: ['5','1','4','4','5','1','2','5','4','1']
+		},
+
+		{
+			name: 'Dayana',
+			scores: ['3','4','1','1','2','2','5','1','1','5']
+		},
+
+		{
+			name: 'Ashley',
+			scores: ['2','4','2','4','4','4','1','1','1','5']
+		},
+
+
+
+
+	]
+}
